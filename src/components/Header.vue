828461a9609ca42e3419c072eb44c9c3ddd8cce9
LaCovLoid <tnsgus0018@naver.com>
2025-03-29 00:00:00 +0900

    버튼으로 최상단 alert div 닫게 함

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="header-container">
-    <div class="alert-container">
-      <span class="">
+    <div class="alert-container" :class="{ invisible: isClosed }">
+      <span class="alert-text">
         1Password is now the exclusive Cybersecurity Partner of Oracle Red Bull
         Racing!
       </span>
-      <router-link to="/">Learn more</router-link>
-      <img src="../assets/images/icons/cross.svg" @click="closed()" />
+      <router-link to="/" class="alert-router">Learn more</router-link>
+      <img
+        class="cross-bt"
+        src="../assets/images/icons/cross.svg"
+        @click="closed()"
+      />
     </div>
   </div>
 </template>
@@ -22,4 +26,38 @@ const closed = () => {
 
 <style lang="scss" scoped>
 @use "@/global.scss" as *;
+
+.header-container {
+  min-height: 64px;
+
+  > .alert-container {
+    padding-top: 20px;
+    padding-bottom: 20px;
+
+    position: relative;
+
+    background-color: #ffa2a2;
+    transition: transform 0.5s ease-in-out, opacity 0.5s ease-in-out;
+
+    > .alert-text {
+      font-size: 15.38px;
+      line-height: 24px;
+    }
+    > .alert-router {
+      font-size: 15.5px;
+      line-height: 24px;
+      letter-spacing: -0.32px;
+    }
+    > .cross-bt {
+      position: absolute;
+      right: 5px;
+      top: 0;
+      padding: 20px;
+    }
+  }
+  .alert-container.invisible {
+    transform: translateY(-100%);
+    opacity: 0;
+  }
+}
 </style>
