e4ab88a2aa9a9296c83f6b303b59878920a41259
LaCovLoid <tnsgus0018@naver.com>
2025-03-28 14:49:41 +0900

    기본 resource 이미지 세팅

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,24 @@
 <template>
-  <div class="header-container"></div>
+  <div class="header-container">
+    <div class="alert-container">
+      <span class="">
+        1Password is now the exclusive Cybersecurity Partner of Oracle Red Bull
+        Racing!
+      </span>
+      <router-link to="/">Learn more</router-link>
+      <img src="../assets/images/icons/cross.svg" @click="closed()" />
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, type Ref } from "vue";
+
+const isClosed: Ref<boolean> = ref(false);
+const closed = () => {
+  isClosed.value = true;
+};
+</script>
 
 <style lang="scss" scoped>
 @use "@/global.scss" as *;
