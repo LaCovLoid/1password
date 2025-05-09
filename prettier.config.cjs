9e19541d317d70dc12b867e6157fd50c636f29ba
LaCovLoid <tnsgus0018@naver.com>
2025-05-09 17:38:25 +0900

    prettier 설정 수정

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -7,4 +7,5 @@ module.exports = {
   htmlWhitespaceSensitivity: "ignore", // HTML 줄바꿈 민감도 ↓
   bracketSpacing: true,               // { a: 1 } 같은 중괄호 사이 공백
   arrowParens: "avoid",               // (x) => x → x => x
+  endOfLine: 'auto',
 };
